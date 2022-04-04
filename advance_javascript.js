6d240a4895785f228d5308e9962a7935079bcbd3
Elvis Boves <elvisboves@Elviss-MacBook-Pro.local>
2022-04-04 09:49:59 -0400

    working with functions

---
+++ advance_javascript.js
@@ -0,0 +1,35 @@
+/*
+ 
+ CHALLENGE 1
+
+ */
+
+//const calcAverage = (score1, score2, score3) =>{
+//    return (score1 + score2 + score3)/3;
+//}
+//
+//
+//    
+//function checkWinner(aveDolphin, aveKoala){
+//
+//    if(aveDolphin >= (aveKoala * 2)){
+//        return `Dolphins win (${aveDolphin} vs. ${aveKoala})`
+//    }else if(aveKoala >= (aveDolphin * 2)){
+//        return `Koalas win (${aveKoala} vs. ${aveDolphin})`
+//    }else{
+//        return 'none of the team won'
+//    }
+//}
+//
+//const aveDol1 = calcAverage(44, 23, 71);
+//const aveKoa1 = calcAverage(65, 54, 49);
+//
+//const aveDol2 = calcAverage(85, 54, 41);
+//const aveKoa2 = calcAverage(23, 34, 27);
+//
+//const data1 = checkWinner(aveDol1, aveKoa1);
+//const data2 = checkWinner(aveDol2, aveKoa2);
+//
+//console.log(data1);
+//console.log(data2);
+
